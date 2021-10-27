ad36aefe71dbf0a2c17a1e1061d0aa1d4a142a2a
Adrian Pantea <adrianpantea10@gmail.com>
2021-10-27 16:01:44 +0100

    added home component

---
+++ src/Components/common/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Home = () => {
+
+  return (
+
+    <section className="hero is-fullheight-with-navbar">
+      <div className="hero-body">
+        <div className="container welcome-text">
+          <h1 className="title is-1 has-text-centered">
+            <span className="logo-emoji" role="img" aria-label="logo"></span>
+          Welcome to <br>
+            </br><span className="drinkard fade-in">DRINKARD</span>
+            <Link to="/cocktails">
+              <span className="logo-emoji" role="img" aria-label="logo">🍸</span></Link>
+          </h1>
+        </div>
+        
+      </div>
+
+      
+    </section>
+
+
+  )
+}
+
+export default Home
